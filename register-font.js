d9dcd3da3b328eebd901c7237384defa05ca08e1
SQUARE <indiciumars@gmail.com>
2022-09-15 23:32:42 +0900

    簡体字の対応

--- register-font.js
+++ register-font.js
@@ -1,4 +1,5 @@
 import nc from 'canvas';
 nc.registerFont('./assets/rounded-x-mgenplus-2p-bold.ttf', { family: 'rounded-x-mgenplus-2p-bold' });
 nc.registerFont('./assets/NotoSansMath-Regular.ttf', { family: 'Noto Sans Math' });
+nc.registerFont('./assets/NotoSansSC-Bold.otf', { family: 'Noto Sans SC' });
 nc.registerFont('./assets/NotoSansTamil-Bold.ttf', { family: 'Noto Sans Tamil' });
